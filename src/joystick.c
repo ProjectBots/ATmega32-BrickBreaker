d37f41566a72d54e84e493f534d61431e6c007fc
ProjectBots <real.projectbots@gmail.com>
2025-08-21 20:07:08 +0200

    fixed adc prescaler not being set correctly

--- src/joystick.c
+++ src/joystick.c
@@ -11,11 +11,11 @@ void joystickInit() {
 	BIT_CLR(ADMUX, REFS0);
 	BIT_CLR(ADMUX, REFS1);
 
-	// Set channel to 0 (X-axis)
+	// Set channel to 0
 	ADMUX &= ~0b11111;
 
-	// Enable the ADC
-	ADCSRA = BIT(ADEN);
+	// Enable the ADC, set prescaler to 128 for 125 kHz ADC clock
+	ADCSRA = BIT(ADEN) | BIT(ADPS2) | BIT(ADPS1) | BIT(ADPS0);
 }
 
 void requestJoystickUpdate() {
@@ -27,6 +27,6 @@ uint16_t joystickRead() {
 	// Wait for ADC conversion to complete
 	while (BIT_IS_SET(ADCSRA, ADSC));
 
-	// Joystick is rotated, so we invert the value
+	// Joystick is physically rotated, we need to invert the ADC value
 	return 1024 - ADC;
 }
